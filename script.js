c44a61b1683081a6a2ff833675d21ab99bc9a6d4
Irina Plaksina <irene.plaksina@gmail.com>
2020-04-03 01:11:27 -0400

    Expanded arrays

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ function generatePassword() {
   //randomly choose the datatype for each chars
 
   let charSet = {
-    "lowerCaseArr": ["a", "b", "c", "d", "e"],
-    "upperCaseArr": ["F", "G", "H", "E", "I"],
-    "specialCharArr": ["~", "!", "&", "$", "?"],
-    "numCharArr": [0, 3, 4, 8, 9]
+    "lowerCaseArr": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    "upperCaseArr": ["A", "B", "C", "D", "E", "F", "G", "H", "E", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    "specialCharArr": ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "?"],
+    "numCharArr": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   };
 
   //generate the password based on the user's choice, fill it with randomly chosen types of chars
